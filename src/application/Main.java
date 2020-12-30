d7225d0ebfe5b3dab0b1585bd5ac0f956cc9d13b
LucaFalasca <30274870+LucaFalasca@users.noreply.github.com>
2020-12-30 16:17:10 +0100

    commit main

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,10 +15,10 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		//commento
 	}
 	
 	public static void main(String[] args) {
